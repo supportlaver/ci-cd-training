3d8490bea17097229f1f1c39bf9b0df0b3514485
supportlaver <ak4686@naver.com>
2024-04-02 15:44:56 +0900

    jijijijijiji

--- src/main/java/com/example/cicd/TestController.java
+++ src/main/java/com/example/cicd/TestController.java
@@ -8,11 +8,11 @@ public class TestController {
 
     @GetMapping("/api/test")
     public String test() {
-        return "test is OOKK";
+        return "test is OOKK!!!!!!!!!";
     }
 
     @GetMapping("/api/test/jiwon")
     public String test2() {
-        return "test is JIWONJIWON2222";
+        return "test is JIWONJIWON2222"!!!!!!!!;
     }
 }
